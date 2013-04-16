d0010a1e8072fadbe21201705a0c3a874c780ee0
R. Vinob chander <vinobchanderr@ssn.edu.in>
2013-04-17 05:12:03 +0530

    added a Users controller and layout links integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 ThirdRorApp::Application.routes.draw do
 
+  get "users/new"
+
   root :to=>"pages#home"
   match '/contact', :to=>'pages#contact'
   match '/about', :to=>'pages#about'
   match '/support', :to=>'pages#support'
+  match '/signup', :to=>'users#new'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.

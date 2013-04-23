bf66fc3884dbed76933441e7182f0bd707ca2697
R. Vinob chander <vinobchanderr@ssn.edu.in>
2013-04-23 10:14:28 +0530

    editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  
   def show
     @user=User.find(params[:id])
     @title=@user.name
   end
+  
   def new
     @user = User.new
     @title="Sign up"
   end
+  
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -17,4 +20,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user=User.find(params[:id])
+    @title="Edit user"
+  end
+  
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash=>{:success=>"Profile updated"}
+    else
+      @title="Edit user"
+      render 'edit'
+    end
+  end
+     
 end

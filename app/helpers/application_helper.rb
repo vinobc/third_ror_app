d80e1041fed867ff81731c6a5881e8f147e87e47
R. Vinob chander <vinobchanderr@ssn.edu.in>
2013-04-26 05:24:24 +0530

    modified application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   #Return a title on a per page basis
   def title
-    base_title="Ruby on Rails Example Application"
+    base_title="tiblo"
     if @title.nil?
       base_title
     else
